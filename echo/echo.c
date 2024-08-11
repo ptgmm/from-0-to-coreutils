96853bd1466749fdaa3250396a473a0d81a65f21
ptgmm <ptgmm@proton.me>
2024-08-12 01:47:45 +0200

    echo

---
+++ echo/echo.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main (int argc, char *argv[])
+{
+    if (argc > 1){
+        printf("%s \n", argv[1]);
+    }
+    else {
+        printf(" \n");
+    }
+
+
+    return 0;
+}
