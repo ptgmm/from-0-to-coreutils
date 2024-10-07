8ffb9a76b7812e904e48b4f4e4e2577547ac6b6e
ptgmm <ptgmm@proton.me>
2024-10-07 21:43:13 +0200

    Print more than one word

--- echo/echo.c
+++ echo/echo.c
@@ -1,14 +1,16 @@
 #include<stdio.h>
 
-int main (int argc, char *argv[])
-{
-    if (argc > 1){
-        printf("%s \n", argv[1]);
+int main (int argc, char *argv[]) {
+
+    int i;
+    if (argc > 1) { 
+        for (i = 1; i < argc; i ++) {
+            printf("%s ", argv[i]);
     }
+}
     else {
         printf(" \n");
     }
-
 
     return 0;
 }
